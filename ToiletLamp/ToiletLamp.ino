a4865c3458c64d29a7ea5136a8ecde4b96d11a19
Niek Arends <niek@civity.nl>
2017-10-28 15:22:53 +0200

    Start of lamp project

---
+++ ToiletLamp/ToiletLamp.ino
@@ -0,0 +1,153 @@
+/*
+  Web client
+
+  This sketch connects to a website (http://www.google.com)
+  using a WiFi shield.
+
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the WiFi.begin() call accordingly.
+
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the WiFi.begin() call accordingly.
+
+  Circuit:
+   WiFi shield attached
+
+  created 13 July 2010
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
+*/
+
+
+#include <SPI.h>
+#include <WiFi101.h>
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+const int redPin = 0;     // the number of the pushbutton pin
+const int greenPin = 1;
+const int bluePin = 1;
+
+int currentState = 0;         // variable for reading the pushbutton status
+int previousState = 0;
+
+
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+IPAddress server(185, 170, 7, 129); // numeric IP for Google (no DNS)
+//char server[] = "www.google.com";    // name address for Google (using DNS)
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  // initialize the pushbutton pin as an input:
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+}
+
+void connectToWiFi() {
+  WiFi.maxLowPowerMode();
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(5000);
+  }
+  Serial.println("Connected to wifi");
+  printWiFiStatus();
+}
+
+String doGet() {
+  Serial.println("\nStarting connection to server...");
+  if (client.connect(server, 1026)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /v2/entities/sensor1/attrs/status/value HTTP/1.1");
+    client.println("Host: 185.170.7.129:1026");
+    client.println("Connection: close");
+    client.println();
+    Serial.println("Sent request, now starting reading");
+  }
+  char s[] = "";
+  //Read the response
+
+  while (client.connected()) {
+    int i = 0;
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+    }
+  }
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+  }
+  //  Serial.println("Response was [" + s + "]");
+}
+
+void loop() {
+  //Check if still connected to WIFI
+  status = WiFi.status();
+  if (status != WL_CONNECTED) {
+    Serial.println("Lost Wifi connection");
+    connectToWiFi();
+  }
+
+  //Check button1 state
+  doGet();
+  
+  if (currentState != previousState) {
+    previousState = currentState;
+    if (currentState == 1) {
+      Serial.println("Toilet 1 occupied");
+      //      WiFi.end();
+    } else {
+      Serial.println("Toilet 1 free");
+    }
+  }
+
+  //Only check once a second
+  delay(1000);
+}
+
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI): ");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
---
+++ ToiletLamp/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_SSID "It hurts when IP 2.4Ghz"
+#define SECRET_PASS "appelsap1"
+
